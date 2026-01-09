5607b3afc722ed0987174633e17e2763247d2d48
Michal <miso.zima@1337tester.com>
2026-01-09 11:33:10 +0100

    test of sync

---
+++ exercises/12_options/options1.rs
@@ -0,0 +1,47 @@
+// This function returns how much icecream there is left in the fridge.
+// If it's before 22:00 (24-hour system), then 5 scoops are left. At 22:00,
+// someone eats it all, so no icecream is left (value 0). Return `None` if
+// `hour_of_day` is higher than 23.
+fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
+    // Return None for invalid hours (>23), 0 at/after 22:00, otherwise 5.
+    if hour_of_day > 23 {
+        None
+    } else if hour_of_day >= 22 {
+        Some(0u16)
+    } else {
+        Some(5u16)
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+    let number: Option<u16> = Some(2);
+    if let Some(i) = number {
+        println!("Matched {:?}!", i);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn raw_value() {
+        // TODO: Fix this test. How do you get the value contained in the
+        // Option?
+        let icecreams = maybe_icecream(12);
+
+        assert_eq!(icecreams.unwrap(), 5); // Don't change this line.
+    }
+
+    #[test]
+    fn check_icecream() {
+        assert_eq!(maybe_icecream(0), Some(5));
+        assert_eq!(maybe_icecream(9), Some(5));
+        assert_eq!(maybe_icecream(18), Some(5));
+        assert_eq!(maybe_icecream(22), Some(0));
+        assert_eq!(maybe_icecream(23), Some(0));
+        assert_eq!(maybe_icecream(24), None);
+        assert_eq!(maybe_icecream(25), None);
+    }
+}
