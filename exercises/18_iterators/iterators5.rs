deba34439d6e662b6df0729dd8a70150f2f48cde
Michal <miso.zima@1337tester.com>
2026-01-13 09:56:12 +0100

    iter5 and smart pointers

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -4,7 +4,7 @@
 // number of exercises with a given progress. Recreate this counting
 // functionality using iterators. Try to not use imperative loops (for/while).
 
-use std::collections::{HashMap, btree_map::Keys};
+use std::collections::{HashMap};
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum Progress {
@@ -49,7 +49,11 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
-    return 3
+    collection
+        .iter()
+        .flat_map(|map| map.values())
+        .filter(|&&v| v == value)
+        .count()
 }
 
 fn main() {
